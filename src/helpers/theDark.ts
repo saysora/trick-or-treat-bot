2de7b66303d215e0ad0a7fd7b47aeac2fb9b8066
saysora <braden@devskull.com>
2025-09-29 18:38:54 -0700

    Ensure player comes with the reload

--- src/helpers/theDark.ts
+++ src/helpers/theDark.ts
@@ -33,7 +33,7 @@ export async function setTarget(currentTarget: string | null) {
   }
 
   await theDark.save();
-  await theDark.reload();
+  await theDark.reload({include: {model: Player}});
 
   return theDark;
 }
